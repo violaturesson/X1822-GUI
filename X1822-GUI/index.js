b3a358c776164d2442f13b7a94f0c1ada39ab7ce
Viola Turesson <violaturesson@gmail.com>
2018-02-02 17:22:33 +0100

    add index files

---
+++ X1822-GUI/index.js
@@ -0,0 +1,9 @@
+﻿var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        document.getElementById("notes").innerHTML = xhttp.responseText;
+    }
+};
+xhttp.open("GET", "http://localhost:8088/notes/5a748ac47aaa3009a0a0340f", true);
+xhttp.send();
